1b6bd928964a08f6d5a43a51b9a68f1758e61029
Mob <162936851+pyMob@users.noreply.github.com>
2024-03-11 01:29:37 -0600

    Add files via upload

---
+++ MobMathModule.py
@@ -0,0 +1,11 @@
+import math 
+
+x = 9.9
+
+# print(math.pi) #periphery
+# print(math.e) #exponential constant
+# result = math.sqrt(x)
+# result = math.ceil(x)
+# result = math.floor(x)
+
+# print(result)
