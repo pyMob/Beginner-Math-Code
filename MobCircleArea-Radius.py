41446c469dc9e937cbc8ae494dea87f2a1f88fad
Mob <162936851+pyMob@users.noreply.github.com>
2024-03-11 01:28:08 -0600

    Add files via upload

---
+++ MobCircleArea-Radius.py
@@ -0,0 +1,13 @@
+import math
+
+#radius = float(input('Enter the radius of a circle: '))
+
+#circumference = 2 * math.pi * radius
+
+#print(f"the circumference is: {circumference, 2}")
+
+radius = float(input("Enter the radius of a circle: "))
+
+area = math.pi * pow(radius, 2)
+
+print(f"the area of the circle is: {round(area)}")
